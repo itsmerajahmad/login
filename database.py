f1f77af617350fed634b8df614e9e03fb8545274
itsmerajahmad <36411743+itsmerajahmad@users.noreply.github.com>
2018-08-01 17:24:03 +0530

    databse user changed

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import pymysql
 from config import con,cur
-con = pymysql.connect(user='root',host='localhost',database='demo')
+con = pymysql.connect(user='be66e459d6f8f5',password='50eb0638',host='us-cdbr-iron-east-01.cleardb.net',database='heroku_9d30db5356467f4')
 cur = con.cursor()
 
